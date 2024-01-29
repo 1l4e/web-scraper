2170154d7cb77152f55a225f8043a16072ccf060
ANGEL\angel <angelinmyeye@outlook.com>
2024-01-29 13:57:57 +0700

    add:pm2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()
-	]
+	],
+	resolve: {
+		alias: {
+			".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+		}
+	}
 });
